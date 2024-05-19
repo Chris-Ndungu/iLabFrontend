a9f51325dc1713e62964eadb98a1fe1602be023c
Chris Ndungu <128899227+Chris-Ndungu@users.noreply.github.com>
2024-05-19 19:38:24 +0300

    Added header feature

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HeaderSection from './components/HeaderSection.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <HeaderSection />
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
-
+<!-- 
 <style scoped>
 header {
   line-height: 1.5;
@@ -44,4 +37,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    let open = ref(false)
+    let Links = [
+      { name: 'Home', link: '/' },
+      { name: 'Services', link: '/talks' },
+      { name: 'About', link: '/about' },
+      { name: 'Contact', link: '/contact' }
+    ]
+
+    // change value for menu icon
+    function menuOpen() {
+      open.value = !open.value
+    }
+
+    return { Links, open, menuOpen }
+  }
+}
+</script>
+<template>
+  <nav class="bg-blue-200 pt-2 flex justify-between z-20 md:pr-5">
+    <div class="z-30 content-center">
+      <!-- Logo -->
+      <div class="flex content-center">
+        <img src="@/assets/img/logo.png" alt="" class="h-18 w-24" />
+        <h1 class="text-2xl text-black md:max-w-20">Cancer Center</h1>
+      </div>
+      <!-- Nav bar icon-->
+      <span
+        @click="menuOpen()"
+        class="absolute md:hidden right-5 top-2 cursor-pointer text-5xl text-black"
+      >
+        <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
+      </span>
+    </div>
+
+    <div
+      class="h-screen md:h-max absolute md:static z-10 bg-blue-200 w-full md:w-max flex flex-col content-center md:flex-row md:gap-6 pt-20 md:pt-2"
+      :class="[open ? 'left-0' : 'left-[-100%]']"
+    >
+      <ul class="list-none text-black text-xl md:flex">
+        <li class="text-center p-3" v-for="link in Links" :key="link.name">
+          <a class="text-xl font-semibold" :href="link.link">{{ link.name }}</a>
+        </li>
+      </ul>
+      <div class="text-center pt-5 flex justify-center gap-4">
+        <a href="" class="text-black text-2xl">Login</a>
+        <a href="" class="text-black text-2xl">Sign In</a>
+      </div>
+    </div>
+  </nav>
+</template>
